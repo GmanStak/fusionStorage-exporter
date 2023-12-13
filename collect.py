00359a53cbaad0c6f702e609b0f511d3378f653e
GmanStak <836375453@qq.com>
2023-12-13 11:25:44 +0800

    Update collect.py
    增加token是否可用校验，不可用删除token和session并重新登陆写入新Token

--- collect.py
+++ collect.py
@@ -46,9 +46,47 @@ def api2metrics():
     account_name = account_list
     clear_metrics()
     Fusion = FusionStorage(host=hw_host,port=hw_port,username=hw_username,password=hw_password)
-    Fusion.login()
-    poolInfo(Fusion)
-    accountInfo(Fusion,account_name)
-    clusterReadWriteBan(Fusion)
-    Fusion.logout()
+    has_executed = Fusion.testToken()
+    if has_executed is False:
+        Fusion.logout()
+        Fusion.login()
+    global poolId
+    global poolName
+    pool_data = Fusion.get_pool_info()
+    for storagePools in pool_data['storagePools']:
+        poolId = storagePools['poolId']
+        poolName = storagePools['poolName']
+        totalCapacity = storagePools['totalCapacity']
+        usedCapacity = storagePools['usedCapacity']
+        poolStatus = storagePools['poolStatus']
+        hw_nodepool_status.labels(id=poolId,name=poolName).set(poolStatus)
+        hw_nodepool_total_hdd_cap.labels(id=poolId,name=poolName).set(totalCapacity)
+        hw_nodepool_used_hdd_cap.labels(id=poolId,name=poolName).set(usedCapacity)
+
+    account_data = Fusion.get_account_info(account_name)
+    for info in data:
+        accountId = info['accountId']
+        quotaCapacity = info['quotaCapacity']
+        SpaceSize = info['SpaceSize']
+        Quota = info['Quota']
+        BucketCount = info['BucketCount']
+        ObjectCount = info['ObjectCount']
+        hw_account_id.labels(id=accountId).set(1)
+        hw_account_quotaCapacity.labels(id=accountId).set(quotaCapacity)
+        hw_account_SpaceSize.labels(id=accountId).set(SpaceSize)
+        hw_account_Quota.labels(id=accountId).set(Quota)
+        hw_account_BucketCount.labels(id=accountId).set(BucketCount)
+        hw_account_ObjectCount.labels(id=accountId).set(ObjectCount)
+
+    bandwith_data = Fusion.get_cluster_performance()
+    for bandwith in bandwith_data['data']:
+        indicatorValues = bandwith['indicatorValues']
+        if bandwith['indicator'] == '123':
+            hw_cluster_read_bandwidth.labels(id=poolId,name=poolName).set(indicatorValues)
+        elif bandwith['indicator'] == '124':
+            hw_cluster_write_bandwidth.labels(id=poolId,name=poolName).set(indicatorValues)
+    # poolInfo(Fusion)
+    # accountInfo(Fusion,account_name)
+    # clusterReadWriteBan(Fusion)
+    
 

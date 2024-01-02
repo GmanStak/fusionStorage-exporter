ad66e84035a9c39f86c47d34fa295a5d9f52765f
GmanStak <836375453@qq.com>
2024-01-02 13:53:52 +0800

    Update main.py
    增加访问认证

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from gevent import pywsgi
 from metrics import *
 from collect import api2metrics
 from config import get_args
+from flask_basicauth import BasicAuth
 
 args = get_args()
 
 app = Flask(__name__)
 
+app.config["BASIC_AUTH_USERNAME"] = 'admin'
+app.config["BASIC_AUTH_PASSWORD"] = '123456'
+basic_auth = BasicAuth(app)
+
 @app.route('/')
+@basic_auth.required
 def index():
     return "<h1>Customized Exporter</h1><br><a href='metrics'>Metrics</a>"
 @app.route('/metrics')
+@basic_auth.required
 def fusion_info():
     api2metrics()
     return Response(prometheus_client.generate_latest(REGISTRY),mimetype="text/plain")
